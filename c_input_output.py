738c9984501faed3cfe99a8440aa601f88f60441
mostafa-asefy <asefi.v@gmail.com>
2020-10-21 15:20:24 +0330

    L05 - Input/Output Axis

--- c_input_output.py
+++ c_input_output.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 
-points = [0, 1, 4, 9, 16, 25]
+values = [1, 2, 3, 4, 5]
+square_values = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
 
-ax.plot(points, linewidth=3)
+ax.plot(values, square_values, linewidth=3)
+
+# region Chart Title, Label, Tick, ...
 ax.set_title("Square Numbers", fontsize=22)
 ax.set_xlabel("Value", fontsize=16)
 ax.set_ylabel("Square of Value", fontsize=16)
 ax.tick_params(axis='both', labelsize=12)
+# endregion
 
 plt.show()
